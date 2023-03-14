528c6f33dbda5ce53c6d7b726e431d33ebba1314
bconti123 <bconti00@gmail.com>
2023-03-13 21:09:37 -0700

    Added dropdown list under search bar.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
 	let results = [];
-	// Still working.
+
 	if (str.length === 0) return []; // if str value is empty
 
 	fruit.filter(word => {
@@ -20,23 +22,32 @@ function search(str) {
 }
 
 function searchHandler(e) {
-	showSuggestions(search(input.value), input.value); // replace "App" with input.value later...
+	showSuggestions(search(input.value), input.value); 
 }
 
-function showSuggestions(results, inputVal) {
-	// Still working
-	if (results.length === 0) suggestions.innerHTML = "";
-	
+function showSuggestions(results, inputVal) { 
+
+	suggestions.innerHTML = "";
+
 	results.forEach(str => {
-		const li = document.createElement("li");
-		li.innerHTML = str;
-		suggestions.append(li);
+			const li = document.createElement("li");
+			li.innerHTML = str + str[inputVal.length - 1]; // Still figure out how to add bold in each letter.
+			suggestions.append(li);
 	}); 
 }
 
 function useSuggestion(e) {
-	// TODO
+	if (e.target.tagName === "HTML" || e.target.tagName == "BODY" || e.target.tagName == "INPUT" || e.target.tagName == "UL") {
+		suggestions.innerHTML = "";
+	}
+	else if (e.target = "suggestions") {
+		input.value = e.target.innerText;
+		suggestions.innerHTML = "";
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
+body.addEventListener('click', useSuggestion);
+html.addEventListener('click', useSuggestion);
+
