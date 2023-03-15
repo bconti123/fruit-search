3d90df5215a1d42accc61f3557af8df1407e5118
bconti123 <bconti00@gmail.com>
2023-03-15 15:48:41 -0700

    Clean up unnecessary lines

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const suggestions = document.querySelector('.suggestions ul');
 const html = document.querySelector("html");
 const body = document.querySelector("body");
 
-
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
@@ -36,19 +35,6 @@ function showSuggestions(results, inputVal) {
 			li.innerHTML = str.slice(0, str.toLowerCase().indexOf(inputVal.toLowerCase())) + bold;
 			suggestions.append(li);
 	}); 
-	// results.forEach(str => {
-	// 	const li = document.createElement("li");
-	// 	let boldString = (s, sub) => s.replace(RegExp(sub, 'g'), `<strong>${sub}</strong>`);
-	// 	li.innerHTML = boldString(str.toLowerCase(), inputVal.toLowerCase());
-	// 	suggestions.append(li);
-	// }); 
-	// results.forEach(str => {
-	// 	const li = document.createElement("li");
-	// 	let bold = '<strong>' + str.slice(str.toLowerCase().indexOf(inputVal.toLowerCase())) + '</strong>';
-	// 	li.innerHTML = bold;
-	// 	suggestions.append(li);
-	// }); 
-
 }
 
 function useSuggestion(e) {
@@ -65,24 +51,4 @@ input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
 html.addEventListener('click', useSuggestion);
 body.addEventListener('click', useSuggestion);
-// let a = "Avocado";
-// let b = "oca";
-
-
-// let str = "Hello"
-// let substr = "el"
-// const boldString = (s, subs) => s.replace(RegExp(substr, 'g'), `<strong>${sub}</strong>`);
-
-// results.forEach(str => {
-// 	const li = document.createElement("li");
-// 	li.innerHTML = boldString(str, inputVal);
-// 	suggestions.append(li);
-// }); 
-
-// reference: https://stackoverflow.com/questions/29896907/bold-part-of-string
-
-let a = "dragonfruit"
-let b = "gon"
-
-let cut = a.slice(a.indexOf(b), b.length+b.length);
 
